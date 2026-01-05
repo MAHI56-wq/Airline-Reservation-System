e37f88060d4608dfceeb7c45b5e9c0d23c7a9b01
Shariar Islam Mahi <sshariarislammahi@gmail.com>
2026-01-05 17:03:25 +0600

    Add files via upload

---
+++ Utils.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Utils {
+    public static void emergencyHelp(){
+        JOptionPane.showMessageDialog(null,"Emergency booking: 1.5x price applied!");
+    }
+}
+
+
